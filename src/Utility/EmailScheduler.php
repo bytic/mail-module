cde14d25170783b963bce43a415255f542a7f994
Gabriel Solomon <hello@gabrielsolomon.ro>
2025-02-16 22:53:32 +0200

    fix: email scheduler

--- src/Utility/EmailScheduler.php
+++ src/Utility/EmailScheduler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Nip\MailModule\Utility;
 
+use Bytic\Scheduler\Events\EventAdder;
 use Bytic\Scheduler\Scheduler;
 use Nip\MailModule\Console\Commands\EmailsCleanupData;
 use Nip\MailModule\Console\Commands\EmailsCleanupRecords;
@@ -14,36 +15,45 @@ use Nip\MailModule\Console\Commands\EmailsSend;
  */
 class EmailScheduler
 {
-    public static function schedule(Scheduler $scheduler)
+    public static function schedule(Scheduler $scheduler, $config = []): void
     {
-        self::scheduleSend($scheduler);
-        self::scheduleCleanupData($scheduler);
-        self::scheduleCleanupRecords($scheduler);
+        self::scheduleSend($scheduler, $config);
+        self::scheduleCleanupData($scheduler, $config);
+        self::scheduleCleanupRecords($scheduler, $config);
     }
 
-    public static function scheduleSend(Scheduler $scheduler): void
+    public static function scheduleSend(Scheduler $scheduler, $config = []): void
     {
-        $scheduler
+        $adder = $scheduler
             ->command(EmailsSend::NAME)
             ->everyMinute();
+        self::sheduleCommandConfig($adder, $config);
     }
 
     /**
      */
-    public static function scheduleCleanupRecords(Scheduler $scheduler)
+    public static function scheduleCleanupRecords(Scheduler $scheduler, $config = [])
     {
-        $scheduler
+        $adder = $scheduler
             ->command(EmailsCleanupRecords::NAME)
             ->setHour('1,2,3');
+        self::sheduleCommandConfig($adder, $config);
     }
 
     /**
-     * @return mixed
      */
-    public static function scheduleCleanupData(Scheduler $scheduler)
+    public static function scheduleCleanupData(Scheduler $scheduler, $config = [])
     {
-        $scheduler
-            ->command(EmailsCleanupData::NAME)
+        $adder = $scheduler
+            ->command(EmailsCleanupData::NAME, $config = [])
             ->setHour('1,2,3');
+        self::sheduleCommandConfig($adder, $config);
+    }
+
+    protected static function sheduleCommandConfig(EventAdder $adder, $config)
+    {
+        if (isset($config['using'])) {
+            $adder->using($config['using']);
+        }
     }
 }
